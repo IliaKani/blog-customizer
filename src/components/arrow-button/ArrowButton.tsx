47b01afb9bbed6f9f9e78857cb99c870414d646d
Ilia Kani <kanishchev.ilia@gmail.com>
2024-06-19 13:06:14 +0200

    changed path to relative for deployment

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -1,4 +1,4 @@
-import arrow from 'src/images/arrow.svg';
+import arrow from '../../images/arrow.svg';
 import styles from './ArrowButton.module.scss';
 import { useState } from 'react';
 import clsx from 'clsx';
